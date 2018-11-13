901164ebddfda778b80752e4747dbe856ada1001
Lam Ha Thai <thailh12@gmail.com>
2018-11-13 08:16:45 +0700

    add styled-components

--- scr/components/Counter.js
+++ scr/components/Counter.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { CounterContainer } from '../containers/CounterContainer';
 import { Subscribe } from 'unstated';
-
+import { Title } from '../elements/Title';
 class CounterView extends React.Component {
   render() {
     return (
       <View>
-        <Text>{this.props.data.state.count}</Text>
+        <Title>{this.props.data.state.count}</Title>
         <Button onPress={() => this.props.data.increment()} title="increment" />
         <Button onPress={() => this.props.data.decrement()} title="decrement" />
       </View>
---
+++ scr/elements/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Text } from 'react-native';
+
+export const Title = styled.Text`
+  font-size: 30;
+`;
